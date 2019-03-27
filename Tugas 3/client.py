8146662e3411ac5a817ee69141750b445c9fd438
reynmraw <33017954+reynmraw@users.noreply.github.com>
2019-03-27 18:22:50 +0700

    update

--- Tugas 3/client.py
+++ Tugas 3/client.py
@@ -1,50 +1,170 @@
+
 # Import module
+
 from socket import *
+
 import socket
+
 import threading
-import sys
-import os
 
-class Client(threading.Thread):
-	def __init__(self, no):
-		self.iter = no
-		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		threading.Thread.__init__(self)
+from ast import literal_eval
 
-	def run(self):
-		host = socket.gethostname()
-		port = 9000
-		self.my_socket.sendto('ini data', (host, port))
-		self.my_socket.settimeout(2)
-		size = os.stat('file.jpg').st_size
-		fileno = 0
-		file = open('received_client'+str(self.iter)+'_'+str(fileno)+'.jpg', 'wb+')
-		received = 0
-		while True:
-			try:
-				data, addr = self.my_socket.recvfrom(1024)
-				file.write(data)
-				received += 1
-			except timeout:
-				if received > 0:
-					print "\r client {} received {} of {} " . format(self.iter, received, size)
+import glob
+
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname()
+
+port = 9000
+
+sock.connect((host, port))
+
+
+
+name = raw_input('your name: ')
+
+
+
+while True:
+
+	command = raw_input('your command: ')
+
+	if command[0] == "exit":
+
+		break
+
+
+
+	sock.send(command)
+
+	command = command.split(" ")
+
+
+
+	print '\n=========='
+
+	files = ''
+
+	if command[0] == 'ls':
+
+		print "showing files"
+
+		print '============='
+
+
+
+		data = sock.recv(1024)
+
+		files = literal_eval(data)
+
+
+
+		for filename in files:
+
+			print filename
+
+
+
+	elif command[0] == 'download':
+
+		if len(command) == 2:
+
+			sock.settimeout(1)
+
+			print "downloading files"
+
+			print '================='
+
+
+
+			real = command[1].split("/")
+
+			filename = name+'_'+str(real[-1])
+
+			file = open(filename, 'wb')
+
+			print "Receiving... ", filename
+
+			while True:
+
+				try:
+
+					data = sock.recv(32)
+
+					file.write(data)
+
+				except timeout:
+
+					break
+
+			file.close()
+
+			print "Done Receiving ", filename
+
+
+
+		else:
+
+			print "syntax error"
+
+
+
+	elif command[0] == 'upload':
+
+		if len(command) == 2:
+
+			files = glob.glob("*")
+
+			print "uploading files"
+
+			print '================='
+
+
+
+			filename = command[1]
+
+			if filename in files:
+
+				with open(filename, 'rb') as file:
+
+					print 'Sending to server... '
+
+					while True:
+
+						print 'Sending... {} to server' . format(filename)
+
+						bytes = file.read(1024)
+
+						if not bytes:
+
+							break
+
+						sock.send(bytes)
+
 					file.close()
-					fileno += 1
-					file = open('received_client'+str(self.iter)+'_'+str(fileno)+'.jpg', 'wb+')
-				# self.iter += 1
-				received = 0
-		self.my_socket.close()
 
-def main():
-	client1 = Client(1)
-	client2 = Client(2)
-	# client3 = Client(3)
-	# client4 = Client(4)
+				print "Done Sending", filename
 
-	client1.start()
-	client2.start()
-	# client3.start()
-	# client4.start()
+			else:
 
-if __name__=="__main__":
-	main()
+				print "file not found"
+
+
+
+		else:
+
+			print "syntax error"
+
+
+
+	else:
+
+		print "command not found"
+
+
+
+	print '==========\n'
+
+sock.close()
