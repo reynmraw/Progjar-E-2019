c9389a479efbf01301701b52a069bad17390d3ea
reynmraw <33017954+reynmraw@users.noreply.github.com>
2019-03-27 17:59:56 +0700

    update

--- Tugas 3/server.py
+++ Tugas 3/server.py
@@ -1,54 +1,167 @@
 # Import module
+
 from socket import *
+
 import socket
-import threading
-import sys
-import os
-import time
 
-class ProcessTheClient(threading.Thread):
+from threading import Thread
+
+import glob
+
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+sock.bind((socket.gethostname(), 9000))
+
+sock.listen(1)
+
+
+
+def sendFiles(conn, name, addr):
+
+	files = glob.glob("*")
+
+	ip, port = addr
+
+	try:
+
+		with open(name, 'rb') as file:
+
+			print 'Sending... ', addr
+
+			while True:
+
+				print 'Sending... {} to {}' . format(name, str(port))
+
+				bytes = file.read(1024)
+
+				if not bytes:
+
+					break
+
+				conn.send(bytes)
+
+			file.close()
+
+	except IOError:
+
+		print "file not found"
+
+
+
+def downloadFiles(addr, name):
+
+	ip, port = addr
+
+
+
+	file = open(str(port)+'_'+name, 'wb')
+
+	print "Receiving... ", name
+
+	sock.settimeout(2)
+
+	while True:
+
+		data = sock.recv(32)
+
+		if not data:
+
+			break
+
+		file.write(data)
+
+	file.close()
+
+	print "Done Receiving ", name
+
+
+
+class ProcessTheClient(Thread):
+
 	def __init__(self, connection, address):
+
 		self.connection = connection
+
 		self.address = address
-		threading.Thread.__init__(self)
+
+		Thread.__init__(self)
+
+
 
 	def run(self):
-		file = open('file.jpg', 'rb')
-		data = file.read()
+
 		while True:
-			print "now sending", self.address
-			sent = 0
-			for x in data:
-				self.connection.sendto(x, self.address)
-				sent+=1
-			print "done sending", self.address
-			size = os.stat('file.jpg').st_size
-			print "\r sent {} of {} " . format(sent, size)
-			time.sleep(10)
 
-class Server(threading.Thread):
-	def __init__(self):
-		self.the_clients = []
-		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		threading.Thread.__init__(self)
+			data = self.connection.recv(1024)
 
-	def run(self):
-		host = socket.gethostname()
-		port = 9000
-		self.my_socket.bind((host, port))
-		while True:
-			data, self.client_address = self.my_socket.recvfrom(1024)
-			print >> sys.stderr, 'connection from', self.client_address
+			ip, port = self.address
 
-			clt = ProcessTheClient(self.my_socket, self.client_address)
-			clt.start()
+			data = str(data).split(' ')
 
-			self.the_clients.append(clt)
+			print "{} from client {}".format(data[0], str(port))
 
-def main():
-	svr = Server()
-	svr.start()
 
-if __name__=="__main__":
-	main()
+
+			if data[0] == "ls":
+
+				files = glob.glob("*")
+
+				self.connection.send(str(files))
+
+
+
+			elif data[0] == 'download':
+
+				if len(data) == 2:
+
+					filename = data[1]
+
+					sendFiles(self.connection, filename, self.address)
+
+				else:
+
+					print "syntax error"
+
+
+
+			elif data[0] == 'upload':
+
+				if len(data) == 2:
+
+					filename = data[1]
+
+					downloadFiles(self.address, filename)
+
+					print 'iki'
+
+				else:
+
+					print "syntax error"
+
+
+
+			else:
+
+				self.connection.send('error')
+
+
+
+def server():
+
+	while True:
+
+		print "waiting for request"
+
+		conn, addr = sock.accept()
+
+		clt = ProcessTheClient(conn, addr)
+
+		clt.start()
+
+
+
+server()
